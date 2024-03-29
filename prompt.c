cfc310f9f30a449213a38d99a255a1bdf2493ab8
shankarlohar <shankarloharmail@gmail.com>
2024-03-29 14:38:46 +0530

    feat(prompt.c): Added windows compilation consideration

--- prompt.c
+++ prompt.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* If we are compiling on windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline funciton */
+char *readline(char *propt)
+{
+    fputs(promp, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char *unused) {}
+
+/* Otherwise use the editline header */
+#else
 #include <editline/readline.h>
+#endif
 
 /* Declare a buffer for user of input size 2048 */
 static char input[2048];
